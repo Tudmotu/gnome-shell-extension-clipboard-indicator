dadb366b2181e4ee247f7dd791557c888a4ace4c
olivier DREVET <odrevet@gmail.com>
2014-11-13 10:51:18 +0100

    expose the keyboard shortcuts in a widget

--- prefs.js
+++ prefs.js
@@ -1,3 +1,4 @@
+const GObject = imports.gi.GObject;
 const Gtk = imports.gi.Gtk;
 const Gio = imports.gi.Gio;
 const Lang = imports.lang;
@@ -73,6 +74,14 @@ const App = new Lang.Class({
                 step_increment: 1
             })
         });
+        this.field_keybinding = createKeybindingWidget(SettingsSchema);
+        addKeybinding(this.field_keybinding.model, SettingsSchema, "clear-history",
+                      "Clear history");
+        addKeybinding(this.field_keybinding.model, SettingsSchema, "prev-entry",
+                      "Previous entry");
+        addKeybinding(this.field_keybinding.model, SettingsSchema, "next-entry",
+                      "Next entry");
+
         //this.field_deletion = new Gtk.Switch({
             //active: true
         //});
@@ -97,21 +106,29 @@ const App = new Lang.Class({
             hexpand: true,
             halign: Gtk.Align.START
         });
+        let keybindingLabel  = new Gtk.Label({
+            label: _("Keyboard shortcuts"),
+            hexpand: true,
+            halign: Gtk.Align.START
+        });
+
         //let deleteLabel   = new Gtk.Label({
             //label: _("Enable Deletion"),
             //hexpand: true,
             //halign: Gtk.Align.START
         //});
-        this.main.attach(sizeLabel    , 2, 1, 2 ,1);
-        this.main.attach(previewLabel , 2, 2, 2 ,1);
-        this.main.attach(intervalLabel, 2, 3, 2 ,1);
-        this.main.attach(cacheLabel   , 2, 4, 2 ,1);
+        this.main.attach(sizeLabel       , 2, 1, 2 ,1);
+        this.main.attach(previewLabel    , 2, 2, 2 ,1);
+        this.main.attach(intervalLabel   , 2, 3, 2 ,1);
+        this.main.attach(cacheLabel      , 2, 4, 2 ,1)
+        this.main.attach(keybindingLabel , 2, 5, 2 ,1);
         //this.main.attach(deleteLabel  , 2, 4, 2 ,1);
 
         this.main.attach(this.field_size        , 4, 1, 2, 1);
         this.main.attach(this.field_preview_size, 4, 2, 2, 1);
         this.main.attach(this.field_interval    , 4, 3, 2, 1);
         this.main.attach(this.field_cache_size  , 4, 4, 2, 1);
+        this.main.attach(this.field_keybinding  , 4, 5, 2, 1);
         //this.main.attach(this.field_deletion    , 4, 4, 2, 1);
 
         SettingsSchema.bind(Fields.INTERVAL    , this.field_interval    , 'value' , Gio.SettingsBindFlags.DEFAULT);
@@ -128,3 +145,98 @@ function buildPrefsWidget(){
     let widget = new App();
     return widget.main;
 };
+
+
+//binding widgets
+//////////////////////////////////
+const COLUMN_ID          = 0;
+const COLUMN_DESCRIPTION = 1;
+const COLUMN_KEY         = 2;
+const COLUMN_MODS        = 3;
+
+
+function addKeybinding(model, settings, id, description) {
+    // Get the current accelerator.
+    let accelerator = settings.get_strv(id)[0];
+    let key, mods;
+    if (accelerator == null)
+        [key, mods] = [0, 0];
+    else
+        [key, mods] = Gtk.accelerator_parse(settings.get_strv(id)[0]);
+
+    // Add a row for the keybinding.
+    let row = model.insert(100); // Erm...
+    model.set(row,
+            [COLUMN_ID, COLUMN_DESCRIPTION, COLUMN_KEY, COLUMN_MODS],
+            [id,        description,        key,        mods]);
+}
+
+function createKeybindingWidget(SettingsSchema) {
+    let model = new Gtk.ListStore();
+
+    model.set_column_types(
+            [GObject.TYPE_STRING, // COLUMN_ID
+             GObject.TYPE_STRING, // COLUMN_DESCRIPTION
+             GObject.TYPE_INT,    // COLUMN_KEY
+             GObject.TYPE_INT]);  // COLUMN_MODS
+
+    let treeView = new Gtk.TreeView();
+    treeView.model = model;
+    treeView.headers_visible = false;
+    treeView.expand = true;
+
+    let column, renderer;
+
+    // Description column.
+    renderer = new Gtk.CellRendererText();
+
+    column = new Gtk.TreeViewColumn();
+    column.expand = true;
+    column.pack_start(renderer, true);
+    column.add_attribute(renderer, "text", COLUMN_DESCRIPTION);
+
+    treeView.append_column(column);
+
+    // Key binding column.
+    renderer = new Gtk.CellRendererAccel();
+    renderer.accel_mode = Gtk.CellRendererAccelMode.GTK;
+    renderer.editable = true;
+
+    renderer.connect("accel-edited",
+            function (renderer, path, key, mods, hwCode) {
+                let [ok, iter] = model.get_iter_from_string(path);
+                if(!ok)
+                    return;
+
+                // Update the UI.
+                model.set(iter, [COLUMN_KEY, COLUMN_MODS], [key, mods]);
+
+                // Update the stored setting.
+                let id = model.get_value(iter, COLUMN_ID);
+                let accelString = Gtk.accelerator_name(key, mods);
+                SettingsSchema.set_strv(id, [accelString]);
+            });
+
+    renderer.connect("accel-cleared",
+            function (renderer, path) {
+                let [ok, iter] = model.get_iter_from_string(path);
+                if(!ok)
+                    return;
+
+                // Update the UI.
+                model.set(iter, [COLUMN_KEY, COLUMN_MODS], [0, 0]);
+
+                // Update the stored setting.
+                let id = model.get_value(iter, COLUMN_ID);
+                SettingsSchema.set_strv(id, []);
+            });
+
+    column = new Gtk.TreeViewColumn();
+    column.pack_end(renderer, false);
+    column.add_attribute(renderer, "accel-key", COLUMN_KEY);
+    column.add_attribute(renderer, "accel-mods", COLUMN_MODS);
+
+    treeView.append_column(column);
+
+    return treeView;
+}
