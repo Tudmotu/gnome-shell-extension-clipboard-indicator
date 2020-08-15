2f4d975bb30741a4e1c41deb4eb4b40ec52a0cb9
Robert Mader <robert.mader@posteo.de>
2020-08-15 11:53:29 +0200

    Add simple Makefile
    Move the script files into a Makefile and add a `install` command,
    making development somewhat easier depending on the workflow.
    As the list of files is needed for both installing and bundling,
    make both use a shared list.

--- update-po-files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-xgettext -L Python --from-code=UTF-8 -k_ -kN_ -o clipboard-indicator.pot *.js
-for f in locale/*/LC_MESSAGES/*.po
-do
-    msgmerge $f clipboard-indicator.pot -o $f
-done
