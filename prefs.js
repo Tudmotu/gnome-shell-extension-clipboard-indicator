d6ad2a590bcf62b8ce0d41bab7590bfdeaea474e
Fran Dieguez <frandieguez@gnome.org>
2021-03-20 15:23:21 +0100

    Add support for GNOME 40

--- prefs.js
+++ prefs.js
@@ -48,7 +48,10 @@ const App = new Lang.Class({
     Name: 'ClipboardIndicator.App',
     _init: function() {
         this.main = new Gtk.Grid({
-            margin: 10,
+            margin_top: 10,
+            margin_bottom: 10,
+            margin_start: 10,
+            margin_end: 10,
             row_spacing: 12,
             column_spacing: 18,
             column_homogeneous: false,
@@ -183,8 +186,8 @@ const App = new Lang.Class({
                 let inputWidget = input;
 
                 if (input instanceof Gtk.Switch) {
-                    inputWidget = new Gtk.HBox();
-                    inputWidget.pack_end(input, false, false, 0);
+                    inputWidget = new Gtk.Box();
+                    inputWidget.append(input, false, false, 0);
                 }
 
                 if (label) {
@@ -226,7 +229,8 @@ const App = new Lang.Class({
         SettingsSchema.bind(Fields.STRIP_TEXT, this.field_strip_text, 'active', Gio.SettingsBindFlags.DEFAULT);
         SettingsSchema.bind(Fields.ENABLE_KEYBINDING, this.field_keybinding_activation, 'active', Gio.SettingsBindFlags.DEFAULT);
 
-        this.main.show_all();
+        if (typeof this.main.show_all === 'function')
+            this.main.show_all();
     },
     _create_display_mode_options : function(){
         let options = [{ name: _("Icon") },
